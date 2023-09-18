317c2ba7003fc782167cc135c36146974b1d1e47
Steven Walker <SRWalker@outlook.com>
2023-09-18 14:58:42 +0100

    Added a functional_test with a story

---
+++ functional_tests.py
@@ -0,0 +1,20 @@
+import unittest
+# Functional Test for the Flashcard app
+# The user is called Steven. He opens the app with python
+python 
+# He is greeted with a welcome that asks his name
+# after entering the name, he is asked how many questions he would like, Steven enters 5
+
+# after hitting enter Steven is given a question in the terminal and is asked to answer it.
+
+# the attempt is saved
+# the number of ATTEMPTS is decreased 
+# the attempt is checked against the answer.
+# If correct, Steven is given a congratualtions message.
+
+# The next question is asked and the number of questions to ask is reduced.
+# The number of ATTEMPTS allowed is reset.
+
+
+if __name__ == '__main__':
+    unittest.main() 
