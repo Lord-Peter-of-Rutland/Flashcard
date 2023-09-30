9bd4eedd69f1971b3ebaca750f3acf3f8feba482
Lord-Peter-of-Rutland <117316010+Lord-Peter-of-Rutland@users.noreply.github.com>
2023-09-30 11:20:07 +0100

    Updated flashcard_library.py

--- flashcard_library.py
+++ flashcard_library.py
@@ -1,8 +1,32 @@
-# FLASHCARDS
+# Library of questions for Flashcards
+# CISCO ISO Commands
 import random
 
-# CISCO ISO COMMANDS
-cisco_iso_commands = {'Entre global config mode': 'configure terminal', 'Display current startup configuration' : 'show ruuning config',
+class flashcardLibrary():
+    #! Change to storing  in a database
+    def __init__(self):
+        self.cisco_iso_commands = {'Enter global config mode': 'configure terminal', 'Display current startup configuration' : 'show runing config',
         'Display current running configuration' : 'show running config'}
-        
- 
+    
+    def random_question(self):
+        questions = self.cisco_iso_commands.keys()
+        question = random.choices(list(questions))
+        return question
+    
+    def print_library():
+        questions = flashcardLibrary.cisco_iso_commands.keys()
+        for key in questions:
+            print(f"The question is {key} and has the answer {flashcardLibrary.cisco_iso_commands[key]}" , " is this correct?")
+           
+    def add_question(self, question, answer):
+        self.question_to_add = question
+        self.answer_to_add = answer
+        # Add to the database
+        self.cisco_iso_commands.pop(question, answer)
+    
+    def change_answer(self, correct_answer):
+        pass
+    
+    def delete_question(self, question):
+        pass
+        # self.question_to_delete = 
